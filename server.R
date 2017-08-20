d79b6fc04b87170dc3045d844dd084bcd2a834c0
Gibran Hemani <explodecomputer@gmail.com>
2017-08-20 11:59:38 -0800

    added variable list export function

--- server.R
+++ server.R
@@ -4,33 +4,42 @@ library(alspac)
 data(current)
 data(useful)
 
-current <- subset(current, select=c(name, lab, counts, type, cat2, cat3, cat4))
-useful <- subset(useful, select=c(name, lab, counts, type, cat2, cat3, cat4))
+current <- subset(current, ! name %in% c("aln", "qlet"), select=c(name, lab, counts, type, cat1, cat2, cat3, cat4))
+useful <- subset(useful, ! name %in% c("aln", "qlet"), select=c(name, lab, counts, type, cat1, cat2, cat3, cat4))
+
+dat <- rbind(current, useful)
+names(dat) <- c("Variable", "Details", "Counts", "Type", "Release", "Label 1", "Label 2", "Label 3")
 
 print_rows <- function(s, dat)
 {
 	cat(
-		paste0(dat$name[s], ": ", dat$lab[s]), sep='\n'
+		paste0(dat$Variable[s], ": ", dat$Details[s]), sep='\n'
 	)
 }
 
 shinyServer(function(input, output, session) {
-	output$x3 = DT::renderDataTable(current)
+	output$x3 = DT::renderDataTable(dat)
 	output$x4 = renderPrint({
 	s = input$x3_rows_selected
 	if (length(s)) {
-	  cat('These rows are currently selected:\n\n')
-	  print_rows(s, current)
+		cat('These rows are currently selected:\n\n')
+		print_rows(s, dat)
+		output$makebutton <- renderUI(
+			downloadButton('downloadData', 'Download variable list')
+		)
 	}
 	})
 
-	output$y3 = DT::renderDataTable(useful)
-	output$y4 = renderPrint({
-	s = input$y3_rows_selected
-	if (length(s)) {
-	  cat('These rows are currently selected:\n\n')
-	  print_rows(s, useful)
-	}
-	})
+	output$downloadData <- downloadHandler(
+		filename = function() {
+			paste('data-', Sys.Date(), '.csv', sep='')
+		},
+		content = function(con) {
+			index <- as.numeric(input$x3_rows_selected)
+			write.csv(dat[index,], con, row.names=FALSE)
+		})
 
 })
+
+
+
--- ui.R
+++ ui.R
@@ -4,16 +4,19 @@ shinyUI(fluidPage(
   title = 'ALSPAC variables lookup',
   h1('Browse variables available in ALSPAC'),
   tabsetPanel(
+  	tabPanel("About",
+      fluidRow(
+      	column(4,
+      	  p("Info about this website")
+      	)
+      )
+    ),
     tabPanel("Current", 
       fluidRow(
         column(8, DT::dataTableOutput('x3')),
-        column(4, verbatimTextOutput('x4'))
-      )
-    ),
-    tabPanel("Useful data",
-      fluidRow(
-        column(8, DT::dataTableOutput('y3')),
-        column(4, verbatimTextOutput('y4'))
+        column(4, verbatimTextOutput('x4'),
+        	uiOutput("makebutton")
+        )
       )
     )
   )
