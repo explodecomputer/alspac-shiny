1b4fd4534fa49c53ef322d754a9d1788bbab008b
Gibran Hemani <explodecomputer@gmail.com>
2017-08-22 03:41:56 -0400

    updated

--- ui.R
+++ ui.R
@@ -4,19 +4,20 @@ shinyUI(fluidPage(
   title = 'ALSPAC variables lookup',
   h1('Browse variables available in ALSPAC'),
   tabsetPanel(
-  	tabPanel("About",
-      fluidRow(
-      	column(4,
-      	  p("Info about this website")
-      	)
-      )
-    ),
-    tabPanel("Current", 
+    tabPanel("Variables", 
       fluidRow(
         column(8, DT::dataTableOutput('x3')),
         column(4, verbatimTextOutput('x4'),
         	uiOutput("makebutton")
         )
+      )
+    ),
+  	tabPanel("About",
+      fluidRow(
+      	column(4,
+      	  p("This website can be used to browse the available variables for the ALSPAC data resource."),
+      	  p("To request the variables of interest, select them by clicking on the relevant rows in the table, and then click 'Export'. This will create an Excel file that you can send to the data team.")
+      	)
       )
     )
   )
